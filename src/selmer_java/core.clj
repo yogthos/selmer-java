b86a016945723d5293d8c5fb41c66cd19c2eb5b7
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-07-30 13:20:38 -0400

    added recursive clojurization of Java data structures

--- src/selmer_java/core.clj
+++ src/selmer_java/core.clj
@@ -5,8 +5,20 @@
     :methods [#^{:static true} [render [String java.util.Map] String]
               #^{:static true} [renderFile [String java.util.Map] String]]))
 
+(declare clojurize)
+
 (defn to-map [m]
-  (->> (for [e m] [(keyword (.getKey e)) (.getValue e)]) (into {})))
+  (->> (for [e m]
+         [(keyword (.getKey e)) (clojurize (.getValue e))])
+       (into {})))
+
+(defn clojurize [v]
+  (cond
+    (instance? java.util.Collection v)
+    (map clojurize v)
+    (instance? java.util.Map v)
+    (to-map v)
+    :else v))
 
 (defn -render [^String template ^java.util.Map context]
   (parser/render template (to-map context)))
