f5e95cec29d592d113d2ffa469659d28856d53bb
Yogthos <dmitri.sotnikov@gmail.com>
2013-07-31 00:55:31 -0400

    updated to provide the tag API

--- src/clj/selmer_java/core.clj
+++ src/clj/selmer_java/core.clj
@@ -8,10 +8,10 @@
     :methods [#^{:static true} [render [String java.util.Map] String]
               #^{:static true} [renderFile [String java.util.Map] String]
               #^{:static true} [addFilter [String selmer.extensions.Filter] void]
-              #^{:static true} [addTag ["[Ljava.lang.String;" selmer.extensions.Tag] void]
+              #^{:static true} [addTag [String selmer.extensions.Tag] void]
               ]))
 
-(declare clojurize)
+(declare clojurize javaize)
 
 (defn to-map [m]
   (->> (for [e m]
@@ -26,19 +26,33 @@
     (to-map v)
     :else v))
 
+(defn java-list [coll]
+  (let [l (java.util.ArrayList.)]
+    (doseq [item coll] (.add l item))
+    (javaize l)))
+
+(defn java-map [m]
+  (let [hm (java.util.HashMap.)]
+    (doseq [[k v] m] (.put hm (name k) (javaize v)))
+    hm))
+
+(defn javaize [v]
+  (cond
+   (map? v)
+   (java-map v)
+   (coll? v)
+   (java-list v)
+   :else v))
+
 (defn -addFilter [^String k ^selmer.extensions.Filter f]
   (filters/add-filter! (keyword k) #(.render f %))
   nil)
 
-(defn tag-handler-fn [^selmer.extensions.Tag t]
-  (fn [args context-map]
-    (.render t args context-map)))
-
-(defn -addTag [^"[Ljava.lang.String;" tagNames ^selmer.extensions.Tag t]
-  (let [[open-tag & closing-tags] (map keyword tagNames)]
+(defn -addTag [^String tagName ^selmer.extensions.Tag t]
+  (let [open-tag (keyword tagName)]
     (swap! parser/expr-tags
-           #(assoc % open-tag
-              (apply (partial tag-handler open-tag (tag-handler-fn t)) closing-tags)))
+      assoc open-tag
+           (tag-handler #(.render t (javaize %1) (javaize %2)) open-tag))
     nil))
 
 (defn -render [^String template ^java.util.Map context]
--- src/java/selmer/extensions/Tag.java
+++ src/java/selmer/extensions/Tag.java
@@ -1,10 +1,6 @@
 package selmer.extensions;
 
 public interface Tag {
-  public String render(clojure.lang.IPersistentCollection args,
-                       clojure.lang.IPersistentMap context);
-
-public String render(clojure.lang.IPersistentCollection args,
-                     clojure.lang.IPersistentMap context,
-                     clojure.lang.IPersistentCollection content);
+  public String render(java.util.List args,
+                       java.util.Map context);
 }
