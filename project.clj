a516c75610df41eac5510e28ac4da1ee1602793e
Yogthos <dmitri.sotnikov@gmail.com>
2013-08-30 17:29:53 -0400

    bumped Selmer version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject selmer-java "0.6"
+(defproject selmer-java "0.7"
   :min-lein-version  "2.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [selmer "0.3.6"]]
+                 [selmer "0.4.2"]]
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
   :aot  [selmer-java.core])
