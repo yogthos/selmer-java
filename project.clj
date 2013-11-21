6b731e56c03b840f29e6118b42b52f69af3c6140
Yogthos <dmitri.sotnikov@gmail.com>
2013-11-20 22:48:11 -0500

    added url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject selmer-java "0.9"
   :min-lein-version  "2.0.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/yogthos/selmer-java"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
