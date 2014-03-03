c7eb35473d21f368172d123fdb431bdf306dd32a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-03-03 09:12:54 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [selmer "0.6.0"]]
+                 [selmer "0.6.1"]]
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
   :aot  [selmer-java.core])
