f107f12299b1ee3173b4d057ee9fa049e4128b56
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-05-05 19:33:01 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject selmer-java "0.9"
+(defproject selmer-java "0.91"
   :min-lein-version  "2.0.0"
   :description "Java wrapper for Selmer"
   :url "https://github.com/yogthos/selmer-java"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [selmer "0.6.5"]]
+                 [selmer "0.6.6"]]
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
   :aot  [selmer-java.core])
